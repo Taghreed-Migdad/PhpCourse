b6a5a9fe62b34ec94d939545b9f9d7cc150da556
Taghreed-Migdad <100993596+Taghreed-Migdad@users.noreply.github.com>
2022-08-21 12:18:59 +0300

    commit

---
+++ 02-variables/Style.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+    <?php  include 'footer.php';
+    include 'header.php';
+    ?>
+
+
+</body>
+</html>
---
+++ 02-variables/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+<h1>This is a Heading</h1>
+
+<form>
+  <label for="fname">First name:</label>
+  <input type="text" id="fname" name="fname"><br><br>
+  <label for="lname">Last name:</label>
+  <input type="text" id="lname" name="lname"><br><br>
+  <label for="Password">Password:</label>
+  <input type="Password" id="Password" name="Password"><br><br>
+  <input type="submit" value="Submit">
+</form>
+
+</body>
+</html>
